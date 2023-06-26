8b3971d861e7e6c374cb29a5ceb06c251adf24d1
abra-93 <maskall93@gmail.com>
2023-06-26 16:50:14 +0300

    add

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-// const Dotenv = require("dotenv-webpack");
-module.exports = defineConfig({
-  publicPath: "/weather-app/",
-  transpileDependencies: true,
-  configureWebpack: {},
-});
