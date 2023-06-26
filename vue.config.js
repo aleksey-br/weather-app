a34fb398beb5b53efa86ccf1b4a3fef9eaa91598
abra-93 <maskall93@gmail.com>
2023-06-26 16:14:31 +0300

    change vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const Dotenv = require("dotenv-webpack");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new Dotenv()],
